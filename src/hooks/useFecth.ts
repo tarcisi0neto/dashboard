68098bf97843c03dd9f7bbfc68d8e50e137fdca1
tarcisi0neto <tarcisioxsn@gmail.com>
2022-12-03 15:44:22 -0300

    feat: Adição de catch error

--- src/hooks/useFecth.ts
+++ src/hooks/useFecth.ts
@@ -3,15 +3,23 @@ import axios from 'axios';
 
 export function useFecth<T= unknown>(url: string) {
 
-    const [data, setData] = useState<T | null>(null)
+    const [data, setData] = useState<T | null>(null);
+    const [isFetching, setIsFetching] = useState(true);
+    const [error, setError] = useState<Error | null>(null);
 
     useEffect(() => {
         axios.get(url)
         .then(response => {
             setData(response.data);
         })
+        .catch((error) => {
+            setError(error);
+        })
+        .finally(() => {
+            setIsFetching(false);
+        })
     }, [])
 
 
-    return {data}
+    return {data, error ,isFetching}
 }
