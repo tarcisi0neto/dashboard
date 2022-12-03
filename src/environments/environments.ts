b07ad6a7e0b47ba36b01dca02d556b0db8cec141
tarcisi0neto <tarcisioxsn@gmail.com>
2022-12-03 14:03:56 -0300

    feat: Implementado hooks para fazer as chamadas API

---
+++ src/environments/environments.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    url:'https://633740935327df4c43d22bb2.mockapi.io/api/v1'
+  };
+  
---
+++ src/hooks/useFecth.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export function useFecth<T= unknown>(url: string) {
+
+    const [data, setData] = useState<T | null>(null)
+
+    useEffect(() => {
+        axios.get(url)
+        .then(response => {
+            setData(response.data);
+        })
+    }, [])
+
+
+    return {data}
+}
